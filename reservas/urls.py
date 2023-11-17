350a1c256ac9e276db8f485ecc806855e0afbadf
Endrio Alex Korb <alex.korb@uniasselvi.com.br>
2023-11-17 19:24:06 -0300

    '.'

--- reservas/urls.py
+++ reservas/urls.py
@@ -6,8 +6,6 @@ app_name = "reservas"
 
 urlpatterns = [
     path('laboratorio/',  views.ReservarLaboratorio, name='reservar_laboratorio'),
-    path('laboratorio/',  views.reservarLaboratorioP1, name='reservar_laboratorio_p1'),
-
-
+    path('laboratorio/',  views.reservaLaboratorioP2, name='reservar_laboratorio_p2'),
 ]
 
--- reservas/views.py
+++ reservas/views.py
@@ -18,58 +18,68 @@ def ReservarLaboratorio(request):
                                                             'blocos': blocos,
                                                             'periodos':periodos})
     else:
-        bloco =  request.POST.get('bloco')
-        data = request.POST.get('data')
-        nome_lab = request.POST.get('nome_lab')
-        nome_professor = request.POST.get('nome_professor')
-        periodo = request.POST.get('periodo')
+       return reservaLaboratorioP2(request)
+    # else:
+    #     bloco =  request.POST.get('bloco')
+    #     data = request.POST.get('data')
+    #     nome_lab = request.POST.get('nome_lab')
+    #     nome_professor = request.POST.get('nome_professor')
+    #     periodo = request.POST.get('periodo')
 
-        nome_lab = nome_lab[:-6]
-        lab = Laboratorios.objects.only('id').get(nome=nome_lab).id
-        laboratorio = ReservasLaboratorios.objects.filter(nome_laboratorio=Laboratorios.objects.get(nome=nome_lab)).filter(data_reserva=data).filter(bloco=bloco)
+    #     nome_lab = nome_lab[:-6]
+    #     lab = Laboratorios.objects.only('id').get(nome=nome_lab).id
+    #     laboratorio = ReservasLaboratorios.objects.filter(nome_laboratorio=lab).filter(data_reserva=data).filter(bloco=bloco)
         
-        if laboratorio:
-            mensagem = f'{nome_lab} já está reservado para data {data} e periodo {periodo}'
-            return render(request, 'reserva_laboratorio.html', {'mensagem': mensagem,
-                                                                'labs': labs,
-                                                                'blocos': blocos,
-                                                                'periodos':periodos})
-        else:
-            reserva = ReservasLaboratorios.objects.create(
-                nome_laboratorio = Laboratorios.objects.get(nome=nome_lab),
-                data_reserva = f'{data}',
-                nome_professor = f'{nome_professor}',
-                periodo = f'{periodo}',
-                bloco = f'{bloco}',
-            )
-            reserva.save()
-            mensagem = 'Reserva registrada com sucesso'
-            return render(request, 'consulta.html', {'mensagem': mensagem})
         
 
-def reservarLaboratorioP1(request):
+    #     if laboratorio:
+    #         mensagem = f'{nome_lab} já está reservado para data {data} e periodo {periodo}'
+    #         return render(request, 'reserva_laboratorio.html', {'mensagem': mensagem,
+    #                                                             'labs': labs,
+    #                                                             'blocos': blocos,
+    #                                                             'periodos':periodos})
+    #     else:
+    #         reserva = ReservasLaboratorios.objects.create(
+    #             nome_laboratorio = Laboratorios.objects.get(nome=nome_lab),
+    #             data_reserva = f'{data}',
+    #             nome_professor = f'{nome_professor}',
+    #             periodo = f'{periodo}',
+    #             bloco = f'{bloco}',
+    #         )
+    #         reserva.save()
+    #         mensagem = 'Reserva registrada com sucesso'
+    #         return render(request, 'consulta.html', {'mensagem': mensagem})
+
+def reservaLaboratorioP2(request):
     bloco = request.POST.get('bloco')
     periodo = request.POST.get('periodo')
     data = request.POST.get('data')
+    
+    bloco = bloco.upper()
 
-    laboratoios = Laboratorios.objects.filter(bloco=bloco)
-    labs_reservados = ReservasLaboratorios.objects.filter(data_reserva=data).filter(periodo=periodo).filter(bloco=bloco)
-
+        
+    laboratorios = Laboratorios.objects.filter(bloco=bloco).order_by('nome')
 
     labs_disponiveis = {}
     lista_labs = []
     info_labs = []
     contador = 0
 
-    for lab in labs_reservados:
-        lab = str(lab)
-        lista_labs.append(lab.split())
-        info_labs = lista_labs[contador]
-        nome_lab = f'{info_labs[0]} {info_labs[1]} {info_labs[2]} {info_labs[3]} {info_labs[4]} '
-        contador += 1
-        labs_disponiveis.pop(nome_lab)
+    for lab in laboratorios:
+        labs_disponiveis[f'{lab}'] = f'{lab}'
 
-    return render (request, 'reserva_laboratorio_p2.html',{'labs_disponiveis': labs_disponiveis,
-                                                     'periodo':periodo,
-                                                     'data':data,
-                                                     'bloco':bloco})
+    labs_reservados = ReservasLaboratorios.objects.filter(bloco=bloco).filter(data_reserva=data).order_by('nome_laboratorio')
+    
+    if labs_reservados:
+        for lab_reservado in labs_reservados:
+            lab_reservado = str(lab_reservado)
+            lista_labs.append(lab_reservado.split())
+            info_labs = lista_labs[contador]
+            nome_lab = f'{info_labs[0]} {info_labs[1]} {info_labs[2]} {info_labs[3]} {info_labs[4]} '
+            contador += 1
+            labs_disponiveis.pop(nome_lab)
+    
+    return render(request, 'reserva_laboratorio_p2.html', {'data':data,
+                                                            'periodo':periodo,
+                                                            'bloco':bloco,
+                                                            'labs_disponiveis': labs_disponiveis})
